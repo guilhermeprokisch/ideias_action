591a2a96172839efca998d7cf9115d78e2f6aef9
guilhermeprokisch <guilherme.prokisch@gmail.com>
2020-08-15 20:11:04 -0300

    Backlinks action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,17 @@ git config user.name $ACTION_NAME
 git config user.email $ACTION_MAIL
 git pull https://${GH_PAT}@github.com/$OWNER/$REPO_NAME.git
 
+rm -rf ./.backlinks
+mkdir .backlinks
 
 FILES=$(find '.' -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';')
 for i in $FILES; do
         for j in $FILES; do
                 if grep -q "\[${i%.md}\](${i%.md})\|[[${i}]]" $j; then
-                        echo "${j%.md} -> ${i%.md}" 
-                        echo ${j%.md} >> ./.backlinks/${i%.md}
+                        if [ "${j%.md}" != "${i%.md}" ]; then
+                                echo "${j%.md} -> ${i%.md}" 
+                                echo ${j%.md} >> ./.backlinks/${i%.md}
+                        fi
                 fi
         done
         #cp "$i" "./.backlinks/"
