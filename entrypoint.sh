6979c934b4195d064b9dfe970db3440191abb87a
guilhermeprokisch <guilherme.prokisch@gmail.com>
2020-08-15 18:59:45 -0300

    adicionado mais secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git init
+git config user.name $ACTION_NAME
+git config user.email $ACTION_MAIL
+git pull https://${GH_PAT}@github.com/$OWNER/$REPO_NAME.git
+
+
 FILES=$(find '.' -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';')
 for i in $FILES; do
         cp "$i" "./.backlinks/"
@@ -7,5 +13,5 @@ done
 
 git add .
 git commit -m 'Backlinks action'
-git push --set-upstream https://${GH_PAT}@github.com/guilhermeprokisch/ideias.wiki.git master
-git push --set-upstream https://${GH_PAT}@github.com/guilhermeprokisch/ideias.git master
+git push --set-upstream https://${GH_PAT}@github.com/$OWNER/$REPO_NAME.wiki.git master
+git push --set-upstream https://${GH_PAT}@github.com/$OWNER/$REPO_NAME.git master
