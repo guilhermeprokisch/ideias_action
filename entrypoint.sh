40c9754431f5eb9a3067d7027546ea3a140eb352
guilhermeprokisch <guilherme.prokisch@gmail.com>
2020-08-15 18:40:19 -0300

    adicionado commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,8 @@ FILES=$(find '.' -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';')
 for i in $FILES; do
         cp "$i" "./.backlinks/"
 done
+
+git add .
+git commit -m 'backlink action'
+git push --set-upstream https://guilhermeprokisch@github.com/guilhermeprokisch/ideias.wiki.git master
+git push --set-upstream https://guilhermeprokisch@github.com/guilhermeprokisch/ideias.git master
