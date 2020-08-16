85684d864aa1856542797356c7950a47709ebec8
guilhermeprokisch <guilherme.prokisch@gmail.com>
2020-08-16 19:41:42 -0300

    WIP: Backlinks Docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-source "./backlinks.sh"
-ls
+#source "./backlinks.sh"
+
+function backlinks {
+
+        for i in *.md; do
+                for j in *.md; do
+                        if grep -q "\[${i%.md}\](${i%.md})\|\[\[${i%.md}\]\]" $j; then
+                                if [ "${j%.md}" != "${i%.md}" ]; then
+                                        echo "${j%.md} -> ${i%.md}"
+                                        echo ${j%.md} >> ./.backlinks/${i%.md}
+                                fi
+                        fi
+                done
+        done
+}
 
 git init
 git config user.name $ACTION_NAME
